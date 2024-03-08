ad6a257f736a3ac93198282d4a9d99f61be5b620
joaomarcelo-dev <joaomarcelolimaoliveira7@gmail.com>
2024-03-08 11:21:29 -0300

    test

---
+++ backend/prisma/migrations/20240308141719_add_table_adms/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Adms" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "photo" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL
+);
