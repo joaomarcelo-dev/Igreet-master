026abaaa8bf31b9caa5b056616eb8cc4ce7ae66d
joaomarcelo-dev <joaomarcelolimaoliveira7@gmail.com>
2024-03-11 19:12:34 -0300

    remove redirect

--- frontend/src/Router/index.tsx
+++ frontend/src/Router/index.tsx
@@ -1,26 +1,26 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import NewAppoinment from "../pages/NewAppoinment";
 import Login from "../pages/Login";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootReducerType } from "../types/Reducers.type";
-import localStorageUtils from "../utils/localStorage.utils";
-import appActions from "../redux/actions/app.actions";
+// import localStorageUtils from "../utils/localStorage.utils";
+// import appActions from "../redux/actions/app.actions";
 
 export default function Router() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
   const { token } = useSelector((state: RootReducerType) => state.app);
   const { pathname } = window.location;
 
   useEffect(() => {
-    const tokenLS = localStorageUtils.token.get();
+    // const tokenLS = localStorageUtils.token.get();
 
-    if (tokenLS) {
-      dispatch(appActions.setToken(tokenLS));
-      navigate('/');
-    }
+    // if (tokenLS) {
+    //   dispatch(appActions.setToken(tokenLS));
+    //   navigate('/');
+    // }
 
     if (!token && pathname !== '/login' && !pathname.startsWith('/new-appoinment')) {
       window.location.href = '/login';
