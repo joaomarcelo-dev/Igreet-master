fe52a278811605fca53e2270f7f96c4d24962810
joaomarcelo-dev <joaomarcelolimaoliveira7@gmail.com>
2024-03-11 15:42:42 -0300

    removendo importação do redux dev tools

--- frontend/src/redux/index.ts
+++ frontend/src/redux/index.ts
@@ -1,7 +1,7 @@
-import { legacy_createStore  as createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { legacy_createStore  as createStore } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducers from './reducers';
 
-const store = createStore(rootReducers, { }, composeWithDevTools(applyMiddleware()));
+const store = createStore(rootReducers);
 
 export default store;
